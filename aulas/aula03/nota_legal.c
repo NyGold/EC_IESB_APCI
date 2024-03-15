d4de2fca278ca88aee3041ce84221e43ce92b516
NyGold <94757321+NyGold@users.noreply.github.com>
2024-03-15 22:38:08 +0000

    continuação do código de nota legal
    adicionando:
    - um tipo struct para armazenar os pedidos, que contêm nome, quantidade e valor.
    - tentei adaptar o jeito que código interagia com o type struct, mas não tive sucesso com pegar o nome completo
    
    notas
    
    - o resto do programa funciona normalmente, retirando algumas partes para depois do primeiro input de um item a nota legal
    
    problemas
    
    - algumas vezes o programa vai rodar e ficar a frente de si mesmo, mostrando prompts que não eram para aparecer naquele instante. Isso também ocorre quando é requisitado colocar um segundo item na nota fiscal (provável ser um bug do próprio compilador).

--- aulas/aula03/nota_legal.c
+++ aulas/aula03/nota_legal.c
@@ -3,35 +3,54 @@
 // demonstrando no console um output de uma nota fiscal com todos os itens
 // junto com sua quantidade e valores
 #include <stdio.h>
-#define LIMITE_CITEM 50
+
+struct item{
+  char nome[50]; 
+  int quantidade;
+  float valor;
+};
 
 int main(void)
 {
-  char item[LIMITE_CITEM];
-  int quantidade = 0;
+  struct item itens[5];
+  int quantidade_MAX = 0;
   float valor = 0;
   float total = 0;
   char continue_value = 's';
+  int numero_de_itens = 0;
 
-  do
-  {
-    printf("coloque o nome do item: ");
-    fgets(item, LIMITE_CITEM, stdin);
-    printf("coloque a quantidade do item: ");
-    scanf("%i", &quantidade);
-    printf("coloque o valor do item: ");
-    scanf("%f", &valor);
-    printf("Deseja colocar mais algum item? (s/n)");
-    scanf("%c", &continue_value);
-  } while(continue_value == 's');
+  for (int i = 0; continue_value == 's' || continue_value == 'S'; i++){
+      printf("Digite o nome do item: ");
+      fgets(itens[i].nome, 50, stdin);
+    
+      printf("Digite a quantidade do item: ");
+      scanf("%d", &itens[i].quantidade);
+    
+      printf("Digite o valor do item: ");
+      scanf("%f", &itens[i].valor);
+      
+      itens[i].valor *= itens[i].quantidade;
+      quantidade_MAX += itens[i].quantidade;
+      total += itens[i].valor;
+      numero_de_itens++;
+      
+      printf("Deseja continuar? (s/n): ");
+      scanf("%s", &continue_value);
+      
+    }
   
   printf("\x1b[33m----------------------------\n");
   printf("     N O T A   L E G A L   \n");
   printf("----------------------------\x1b[0m\n");
   printf("ITEM             QTD   VALOR\n");
-  printf("%-16s %04i \x1b[34m%6.2f\x1b[0m\n", item, quantidade, valor);
+  
+  for (int i = 0; i < numero_de_itens; i++){
+      printf("%-16s %04i \x1b[34m%6.2f\x1b[0m\n", itens->nome, itens->quantidade, itens->valor);
+    }
+  
   printf("\x1b[33m----------------------------\x1b[0m\n");
   printf("TOTAL................. \x1b[31m%-10.2f\x1b[0m\n", total);
+  
 /*
   printf("banana nanica   2   R$ 1.00\n");
   printf("coquinha gelada 1   R$ 2.00\n");
