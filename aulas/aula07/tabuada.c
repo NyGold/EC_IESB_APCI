c7fdefd23293988266af94f3ce00c5feb803470d
NyGold <nycosteixeira@gmail.com>
2024-05-11 17:39:27 -0300

    melhorias no código da aula07 no arquivo tabuada.c

--- aulas/aula07/tabuada.c
+++ aulas/aula07/tabuada.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+#define VALOR_MINIMO 1
+#define VALOR_MAXIMO 100
+
 int main(void)
 {
   int numero, numero_limite, deu_certo;
 
   do 
   {
-    printf("entre com um numero entre 1 e 10: ");
+    printf("entre com um numero entre (%i a %i): ", VALOR_MINIMO, VALOR_MAXIMO);
     deu_certo = scanf("%i", &numero);
     
     // verificação de entradas
@@ -16,7 +19,7 @@ int main(void)
       deu_certo = 0;
       continue;
     }
-    if (numero < 1 || numero > 10) 
+    if (numero < VALOR_MINIMO || numero > VALOR_MAXIMO) 
     {
       printf("\x1b[31mValor invalido. Tente novamente!\x1b[0m\n");
       deu_certo = 0;
@@ -25,26 +28,27 @@ int main(void)
 
     do
       {
-        printf("entre com o numero limite: ");
+        printf("entre com o numero limite (%i a %i): ", VALOR_MINIMO, VALOR_MAXIMO);
         deu_certo = scanf("%i", &numero_limite);
 
         // verificações de entradas
         if (!deu_certo) 
         {
-          printf("\x1b[0m Valor limite invalido. Tente novamente!\x1b[0m\n"); 
+          printf("\x1b[31m2Valor limite invalido. Tente novamente!\x1b[0m\n"); 
           deu_certo = 0;
           continue;
         }
-        if (numero_limite < 1 || numero_limite > 10) 
+        if (numero_limite < VALOR_MINIMO || numero_limite > VALOR_MAXIMO) 
         {
-          printf("\x1b[0mValor limite invalido. Tente novamente!\x1b[0m\n");
+          printf("\x1b[31mValor limite invalido. Tente novamente!\x1b[0m\n");
           deu_certo = 0;
           continue;
         }
       } while (!deu_certo);
+
   } while (!deu_certo);
-    
-  printf("tabuada do %i", numero);
+
+  printf("tabuada do %i\n", numero);
   for (int i = 0; i <= numero_limite; i++)
     {
       printf("%i x %i = %i\n", numero, i, numero * i);
